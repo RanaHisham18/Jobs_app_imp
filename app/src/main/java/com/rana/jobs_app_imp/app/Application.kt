53e65c585fd2feb93d765ba42f12cacc0180a80b
dell <hishamrana00@gmail.com>
2021-06-17 12:51:33 +0200

    some issues in Application.kt

--- app/src/main/java/com/rana/jobs_app_imp/app/Application.kt
+++ app/src/main/java/com/rana/jobs_app_imp/app/Application.kt
@@ -2,6 +2,8 @@ import android.app.Application
 import coil.Coil
 import coil.ImageLoader
 import coil.decode.SvgDecoder
+import com.rana.jobs_app_imp.Di.jobsModule
+import com.rana.jobs_app_imp.Di.networkModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -20,7 +22,8 @@ class Application : Application() {
             androidContext(this@Application)
             modules(
                 listOf(
-
+                    jobsModule,
+                    networkModule
                 )
             )
         }
